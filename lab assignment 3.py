25b1fbefaaae2d54896244c612307d8b0c6421a4
Rishita Turrey <rishitaturrey@gmail.com>
2026-03-12 16:36:25 +0530

    lab assignment 3

--- lab assignment 3.py
+++ lab assignment 3.py
@@ -1,4 +1,48 @@
-num = int(input("Enter number: "))
+#task 1
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(j, end="")
+#     print()
 
-for i in range(1, 11):
-    print(num, "x", i, "=", num * i)
+#Task 2
+# for i in range(1, 6):
+#     for j in range(i):
+#         print(i, end="")
+#     print()
+
+#Task 3
+# for i in range(1, 6):
+#     for j in range(i, 0, -1):
+#         print(j, end="")
+#     print()
+
+#Task 4
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(j % 2, end="")
+#     print()
+
+#Task 5
+# num = 2
+# for i in range(1, 5):
+#     for j in range(i):
+#         print(num, end=" ")
+#         num += 2
+#     print()
+
+#Task 6
+# for i in range(1, 6):
+#     print("*" * i)
+
+#Task 7
+# Upper part (increasing)
+# for i in range(1, 6):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+#
+# # Lower part (decreasing)
+# for i in range(4, 0, -1):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
